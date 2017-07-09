20632e478f0bd9ff7cd2b346650c69d91c5f4e7b
Linus <linuzlp@gmail.com>
2017-07-09 22:51:28 +0200

    Initial commit

---
+++ install.py
@@ -0,0 +1,21 @@
+"""
+File: install.py
+Author: lnus 
+Github: https://github.com/lnus
+Description: Installs all the required things for Mp3DL-Lite
+"""
+
+import os
+
+"""
+Installs correct version of PyTube
+Creates the folder Mp3DL on the C drive
+It puts the mp3.bat script file in there
+It puts the install.py script file in there
+"""
+
+os.system("pip uninstall pytube")
+os.system("pip install pytube==6.2.2")
+os.system("mkdir C:\\Mp3DL")
+os.system("copy Installfiles\\mp3.bat C:\\Mp3DL\\mp3.bat")
+os.system("copy main.py C:\\Mp3DL\\main.py")
---
+++ main.py
@@ -0,0 +1,56 @@
+import sys
+import os
+from pytube import YouTube
+
+class Media(object):
+    def download(self, url):
+        try:
+            yt = YouTube(url)
+        except:
+            print("Could not find video")
+            raise SystemExit
+
+        filename = os.getcwd() + "\\" + yt.filename.replace("!", "").replace("&","").replace(" ", "-") + ".mp4"
+        print(filename)
+
+        # Finds best quality for the video
+        resolutions = ["1080p", "720p", "480p", "360p", "240p"]
+        video_info = yt.filter("mp4")
+        for resolution in resolutions:
+            if resolution in str(video_info):
+                res = resolution
+                break
+
+        # Downloads the video
+        try:
+            video = yt.get("mp4", res)
+            video.download(filename)
+        except Exception as e:
+            print(e)
+            print("Could not download video")
+            raise SystemExit
+
+        return filename
+
+    def convert(self, filename):
+        mp3 = filename.replace("mp4", "mp3") 
+        try:
+            os.system("ffmpeg -i {} {}".format(filename, mp3))
+            os.remove(filename)
+        except:
+            print("Could not convert file")
+            raise SystemExit
+        
+
+if __name__ == '__main__':
+    m = Media()
+    try:
+        video_id = sys.argv[1]
+    except:
+        print("""You need to provide a YouTube URL in order for this to work...
+Syntax: 'mp3 VIDEOID'
+Ex. http://youtube.com/watch?v=VIDEOID""")
+        raise SystemExit
+    url = "http://youtube.com/watch?v={}".format(video_id)
+    filename = m.download(url) 
+    m.convert(filename)
